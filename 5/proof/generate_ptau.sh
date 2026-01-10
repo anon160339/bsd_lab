5ef00704034ad0d6be9e17d63c836061ed9af42d
Anon <@>
2026-01-10 19:03:27 +0100

    circuit now calculates the proper merkle root out of the merkle proof provided leafs

--- 5/proof/generate_ptau.sh
+++ 5/proof/generate_ptau.sh
@@ -2,6 +2,6 @@
 # Already generated ceremonies:
 # https://github.com/iden3/snarkjs?tab=readme-ov-file#7-prepare-phase-2
 # Fake ceremony:
-snarkjs powersoftau new bn128 8 ceremony_phase0.ptau -v 
+snarkjs powersoftau new bn128 12 ceremony_phase0.ptau -v 
 snarkjs powersoftau contribute ceremony_phase0.ptau ceremony_phase1.ptau --name="First contribution Name" -v -e="Random text 1"
 snarkjs powersoftau prepare phase2 ceremony_phase1.ptau ceremony_final.ptau -v
